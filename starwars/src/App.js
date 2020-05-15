f96533370fd728dcffe73c5439ccb8ef88c39a1d
Joey <joey@xm8.ninja>
2020-05-15 12:42:55 -0400

    Done!!!

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,6 +1,8 @@
 import React, { useState, useEffect } from "react";
+import axios from "axios";
 import "./App.css";
-// import axios from 'axios';
+import Character from "./components/Character";
+import styled from "styled-components";
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
@@ -10,15 +12,60 @@ const App = () => {
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
 
-  const url = "https://bit.ly/reactWars"; // I'd like to see if this custom shortened link tracks clicks for API's
+  const url = "https://swapi.dev/api/people/"; // I'd like to see if this custom shortened link tracks clicks for API's https://bit.ly/reactWars
   const [characterData, setCharacterData] = useState({});
   const [currentURL, setCurrentURL] = useState(url);
 
-  return (
-    <div className="App">
-      <h1 className="Header">Characters</h1>
-    </div>
-  );
+  const Buttons = styled.button`
+    background-color: gray;
+    border-color: gray;
+    border-radius: 8px;
+    width: 75px;
+    height: 20px;
+    margin: 5px;
+  `;
+
+  useEffect(() => {
+    axios
+      .get(currentURL)
+      .then((res) => {
+        setCharacterData(res.data);
+      })
+      .catch((err) => {
+        console.log("An error retrieving the data there was. Hrmmm.");
+      });
+  }, [currentURL]);
+
+  if (!characterData.results) {
+    return <h3>While this loads please wait. Yes, hrrrm</h3>;
+  } else {
+    return (
+      <div className="App">
+        <h1 className="Header">Characters</h1>
+        {characterData.results.map((char) => {
+          return <Character char={char} />;
+        })}
+        <Buttons
+          onClick={() => {
+            characterData.previous
+              ? setCurrentURL(characterData.previous)
+              : setCurrentURL(url);
+          }}
+        >
+          Previous
+        </Buttons>
+        <Buttons
+          onClick={() => {
+            characterData.next
+              ? setCurrentURL(characterData.next)
+              : setCurrentURL(url);
+          }}
+        >
+          Next
+        </Buttons>
+      </div>
+    );
+  }
 };
 
 export default App;
--- starwars/src/components/Character.js
+++ starwars/src/components/Character.js
@@ -1,1 +1,43 @@
 // Write your Character component here
+import React from "react";
+import styled from "styled-components";
+
+const Container = styled.div`
+  display: flex;
+  justify-content: space-around;
+  align-content: center;
+`;
+const CharacterContainer = styled.div`
+  border: 2px solid white;
+  margin: 0 auto;
+  width: 60%;
+  background-color: rgba(255, 255, 255, 0.5);
+`;
+
+const CharacterName = styled.h1`
+  font-family: "Ubuntu", sans-serif;
+  text-decoration: none;
+`;
+
+const CharacterStats = styled.p`
+  font-family: "Ubuntu", sans-serif;
+`;
+
+function Character(props) {
+  if (!props.char) {
+    return <h3>Patience You Need</h3>;
+  } else {
+    return (
+      <Container>
+        <CharacterContainer>
+          <CharacterName>{props.char.name}</CharacterName>
+          <CharacterStats>Height: {props.char.height}</CharacterStats>
+          <CharacterStats>Mass: {props.char.mass}</CharacterStats>
+          <CharacterStats>Birth Year: {props.char.birth_year}</CharacterStats>
+        </CharacterContainer>
+      </Container>
+    );
+  }
+}
+
+export default Character;
